67c86cca6e6e49cf32a0926ed6cdbe21acfdf52a
Diego <Diegocortes079@gmail.com>
2023-11-25 16:04:12 -0600

    app

---
+++ tablesStoryboards/Car.swift
@@ -0,0 +1,8 @@
+//
+//  Car.swift
+//  tablesStoryboards
+//
+//  Created by J Antonio Enciso S on 23/09/22.
+//
+
+import Foundation
---
+++ tablesStoryboards/CarTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  CarTableViewCell.swift
+//  tablesStoryboards
+//
+//  Created by J Antonio Enciso S on 23/09/22.
+//
+
+import UIKit
+
+class CarTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
